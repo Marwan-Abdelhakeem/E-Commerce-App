9be77b94f81d099d2295e6054a41255347623bb0
Marwan Abdelhakeem <marwan.abdelhakeem.ahmed@gmail.com>
2024-10-01 21:59:50 +0300

    v10

--- src/utils/email.js
+++ src/utils/email.js
@@ -1,14 +1,18 @@
+import path from "path";
+import dotenv from "dotenv";
 import nodemailer from "nodemailer";
+dotenv.config({ path: path.resolve("./config/.env") });
+
 export const sendEmail = async ({ to = "", subject = "", html = "" }) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: "marwan.abdelhakeem.ahmed@gmail.com",
-      pass: "tgolkpffrczadtqr",
+      user: process.env.GMAIL_AUTH_USER,
+      pass: process.env.GMAIL_USER_PASS,
     },
   });
   const info = await transporter.sendMail({
-    from: "'E-commerce'<marwan.abdelhakeem.ahmed@gmail.com>",
+    from: `'E-commerce'<${GMAIL_AUTH_USER}>`,
     to,
     subject,
     html,
